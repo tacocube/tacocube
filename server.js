b51feae3344c4b6aee5b634c1b6401c631b50d7a
Donghyeon Kim <donghyeon.kim@tacocube.com>
2018-06-08 14:18:18 -0400

    Add dotenv Package

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// Read .env file
+const dotenv = require('dotenv').config()
+if (dotenv.error) {
+    throw dotenv.error
+} else {
+    console.log("Environment varialbes loaded:")
+    console.log(dotenv.parsed)
+}
+
 app.use(express.static('static'))
 app.use(express.static('dist'))
 
