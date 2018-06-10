155830267024f213c584d8102a2b4d3a1de1407c
Donghyeon Kim <donghyeon.kim@tacocube.com>
2018-06-09 21:26:00 -0400

    Template

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const port = process.env.PORT || 3000
 // Read .env file
 const dotenv = require('dotenv').config()
 if (dotenv.error) {
-    throw dotenv.error
+  throw dotenv.error
 } else {
-    console.log("Environment varialbes loaded:")
-    console.log(dotenv.parsed)
+  console.log('Environment varialbes loaded:')
+  console.log(dotenv.parsed)
 }
 
-app.use(express.static('static'))
 app.use(express.static('dist'))
 
-app.listen(port, function() { console.log('Listening on port ' + port) })
+const pug = require('pug')
+const index = pug.compileFile('templates/index.pug')
+app.get('/', (req, res) => res.send(index()))
+
+app.listen(port, function () { console.log('Listening on port ' + port) })
